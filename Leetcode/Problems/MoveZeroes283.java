b3497bc00c74366a24523068575d96ac714c0182
LeoLe101 <phucle@uw.edu>
2020-10-09 17:18:36 -0700

    Added some LC problems for LL

---
+++ Leetcode/Problems/MoveZeroes283.java
@@ -0,0 +1,18 @@
+public class MoveZeroes283 {
+
+    public void moveZeroes(int[] nums) {
+
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            if (left == right && nums[left] != 0) {
+                left++;
+            } else if () {
+
+            }
+        }
+
+    }
+
+}
---
+++ Leetcode/Problems/RemoveDuplicatesFromSortedLL83.java
@@ -0,0 +1,26 @@
+public class RemoveDuplicatesFromSortedLL83 {
+
+    // Iteration Space (1), Time (N)
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        while (temp != null && temp.next != null) {
+            if (temp.val == temp.next.val)
+                temp.next = temp.next.next;
+            else
+                temp = temp.next;
+        }
+        return head;
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        head.next = deleteDuplicates(head.next);
+
+        if (head.val == head.next.val) {
+            return head.next;
+        }
+        return head;
+    }
+}
