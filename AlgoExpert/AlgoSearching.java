48a2d05318e07f53c982a67a166f380c11dd938a
LeoLe101 <phucle@uw.edu>
2020-10-23 22:19:49 -0700

    Checkpoint for remove element LC

--- AlgoExpert/AlgoSearching.java
+++ AlgoExpert/AlgoSearching.java
@@ -6,13 +6,14 @@ import java.util.*;
 public class AlgoSearching {
 
     // Recursion
-    // Space: O(1) but can be O(log N) in some language because of the recursion stack
+    // Space: O(1) but can be O(log N) in some language because of the recursion
+    // stack
     // Time: O(N) because it split the array by half every iteration
-    public int binarySearch(int[] array, int target) {
+    public static int binarySearch(int[] array, int target) {
         return binarySearchHelper(array, target, 0, array.length - 1);
     }
 
-    public int binarySearchHelper(int[] array, int target, int startindx, int endindx) {
+    public static int binarySearchHelper(int[] array, int target, int startindx, int endindx) {
         int midindx = startindx + (endindx - startindx) / 2;
 
         if (startindx > endindx)
@@ -25,6 +26,66 @@ public class AlgoSearching {
         } else {
             return binarySearchHelper(array, target, startindx, midindx - 1);
         }
+    }
+
+    // Time (N) - Space (1)
+    public static int[] findThreeLargestNumbers(int[] array) {
+        int num1 = Integer.MIN_VALUE;
+        int num2 = Integer.MIN_VALUE;
+        int num3 = Integer.MIN_VALUE;
+
+        for (int num : array) {
+            if (num >= num3) {
+                num1 = num2;
+                num2 = num3;
+                num3 = num;
+            } else if (num >= num2 && num < num3) {
+                num1 = num2;
+                num2 = num;
+            } else if (num >= num1 && num < num2) {
+                num1 = num;
+            }
+        }
+        return new int[] { num1, num2, num3 };
+    }
+
+    // This solution use Binary Search to help find the location of the target
+    // Not good solution Time (N LOGN) - Space (N) Due to Binary recursion stack
+    public static int[] searchInSortedMatrix(int[][] matrix, int target) {
+
+        int[] result = new int[] { -1, -1 };
+
+        for (int i = 0; i < matrix.length; i++) {
+            int index = binarySearch(matrix[i], target);
+            if (index > -1) {
+                result[0] = i;
+                result[1] = index;
+                break;
+            }
+        }
+        return result;
+    }
+
+    // Move from the Top Right to the Bottom Left of the Matrix
+    // Constantly shift row and col accordingly to check if the current is the
+    // target
+    // Sapce (1) - Time (N + M) with N is the number of the whole row. M is the
+    // number of the whole col
+    public static int[] searchInSortedMatrixOptimized(int[][] matrix, int target) {
+        int col = 0;
+        int row = matrix[col].length - 1;
+
+        while (row > -1 && col < matrix.length) {
+            int curr = matrix[col][row];
+            if (curr == target) {
+                return new int[] { col, row };
+            } else if (curr > target) {
+                row--;
+            } else {
+                col++;
+            }
+        }
+        return new int[] { -1, -1 };
     }
 
 }
--- Leetcode/Linkedlist/RemoveLLElements203.java
+++ Leetcode/Linkedlist/RemoveLLElements203.java
@@ -4,19 +4,26 @@ public class RemoveLLElements203 {
 
     // Space O(1), Time (N)
     public ListNode removeElements(ListNode head, int val) {
-        if (head == null || head.next == null)
-            return null;
-
-        ListNode slow = head;
-        ListNode fast = head.next;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = head;
 
         while (fast != null) {
             if (fast.val == val) {
                 slow.next = fast.next;
-            }
-            slow = slow.next;
+            } else 
+                slow = slow.next;
             fast = fast.next;
         }
-        return head;
+        return dummy.next;
     }
+
+    public ListNode removeElementsRecursion(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        head = removeElements(head.next, val);
+    }
+
 }
