11cce755d595ca4901037c61c39ed217c480d56a
LeoLe101 <leolemain@gmail.com>
2020-12-18 17:00:34 -0800

    Added LC387

--- Java Interview Practice/src/Leetcode/Problems/LC108ConvertSortedArraytoBST.java
+++ Java Interview Practice/src/Leetcode/Problems/LC108ConvertSortedArraytoBST.java
@@ -2,45 +2,72 @@ package src.Leetcode.Problems;
 
 public class LC108ConvertSortedArraytoBST {
 
-	public TreeNode sortedArrayToBST(int[] nums) {
-		int midIndx = 0 + ((nums.length - 1) - 0) / 2;
+    public TreeNode sortedArrayToBST(int[] nums) {
+        int midIndx = 0 + ((nums.length - 1) - 0) / 2;
 
-		TreeNode root = new TreeNode(nums[midIndx ]);
+        TreeNode root = new TreeNode(nums[midIndx ]);
 
-		// Add all node from the left into the Tree
-		addNodeLeft(root, nums, midIndx - 1);
-		// Add all node from the right into the Tree
-		addNodeRight(root, nums, midIndx + 1);
+        // Add all node from the left into the Tree
+        addNodeLeft(root, nums, midIndx - 1);
+        // Add all node from the right into the Tree
+        addNodeRight(root, nums, midIndx + 1);
 
-		return root;
-	}
+        return root;
+    }
 
-	private void addNodeLeft(TreeNode curr, int[] nums, int leftIndex) {
-		
-		if (leftIndex < 0) return;
+    private void addNodeLeft(TreeNode curr, int[] nums, int leftIndex) {
 
-		if (nums[rightIndex] < curr.val) {
-			curr.left = new TreeNode(nums[leftIndex]);
-			addNodeLeft(curr.left, nums, leftIndex--);
-		} else {
-			curr.right = new TreeNode(nums[leftIndex]);
-			addNodeLeft(curr.right, nums, leftIndex--);
-		}
+        if (leftIndex < 0) return;
 
-		return;
-	}
+        if (nums[rightIndex] < curr.val) {
+            curr.left = new TreeNode(nums[leftIndex]);
+            addNodeLeft(curr.left, nums, leftIndex--);
+        } else {
+            curr.right = new TreeNode(nums[leftIndex]);
+            addNodeLeft(curr.right, nums, leftIndex--);
+        }
 
-	private void addNodeRight(TreeNode curr, int[] nums, int rightIndex) {
-		if (rightIndex < 0) return;
+        return;
+    }
 
-		if (nums[rightIndex] < curr.val) {
-			curr.left = new TreeNode(nums[rightIndex]);
-			addNodeRight(curr.left, nums, rightIndex++);
-		} else {
-			curr.right = new TreeNode(nums[rightIndex]);
-			addNodeRight(curr.right, nums, rightIndex++);
-		}
-		
-		return;
-	}
+    private void addNodeRight(TreeNode curr, int[] nums, int rightIndex) {
+        if (rightIndex < 0) return;
+
+        if (nums[rightIndex] < curr.val) {
+            curr.left = new TreeNode(nums[rightIndex]);
+            addNodeRight(curr.left, nums, rightIndex++);
+        } else {
+            curr.right = new TreeNode(nums[rightIndex]);
+            addNodeRight(curr.right, nums, rightIndex++);
+        }
+
+        return;
+    }
+
+
+    // Space (N) Time (N)
+    public TreeNode sortedArrayToBSTRecur(int[] nums) {
+        if (nums.length < 1) {
+            return null;
+        }
+
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int leftIdx, int rightIdx) {
+
+        if (leftIdx > rightIdx) return null;
+
+        // Find the middle index
+        int midIdx = leftIdx + (rightIdx - leftIdx)/2;
+        TreeNode curr = new TreeNode(midIdx);
+
+        // Recur and add child node to this currNode 
+        curr.left = helper(nums, leftIdx, midIdx - 1);
+        curr.right = helper(nums, midIdx + 1, rightIdx);
+        return curr;
+    }
 }
+
+
+
---
+++ Java Interview Practice/src/Leetcode/Problems/LC387FirstUniqueCharacterInString.java
@@ -0,0 +1,44 @@
+package src.Leetcode.Problems;
+
+public class LC387FirstUniqueCharacterInString {
+
+    // Space (1) Time (N)
+    public int firstUniqChar(String s) {
+        int freq [] = new int[26];
+
+        for(int i = 0; i < s.length(); i ++)
+            freq [s.charAt(i) - 'a'] ++;
+
+        for(int i = 0; i < s.length(); i ++)
+            if(freq [s.charAt(i) - 'a'] == 1)
+                return i;
+        
+        return -1;
+    }
+
+    // Space (1) Time (N)
+    public int firstUniqChar(String s) {
+        if (s.length() < 1) return -1;
+
+        Map<Character, Integer> m = new HashMap<Character, Integer>();
+        char[] arr = s.toCharArray();
+
+        for (char c: arr) {
+            if (m.containsKey(c)) {
+                m.put(c, m.getOrDefault(c, 0) + 1);
+            } else {
+                m.put(c, 1);
+            }
+        }
+
+
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+
+            if (m.containsKey(c) && m.getOrDefault(c, 0) == 1) return i;
+        }
+
+        return -1;
+    }
+}
+
