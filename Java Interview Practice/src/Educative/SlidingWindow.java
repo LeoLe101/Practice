152fce05dfd907f82af96d82a6f12033a4d16a16
LeoLe101 <leolemain@gmail.com>
2021-01-07 00:07:52 -0800

    added Educative and Leetcode Longest Repeating Char Replacement

--- Java Interview Practice/src/Educative/SlidingWindow.java
+++ Java Interview Practice/src/Educative/SlidingWindow.java
@@ -287,7 +287,32 @@ public class SlidingWindow {
                 maxSub = Math.max(maxSub, s.size());
             }
         }
+        return maxSub;
+    }
 
+    // #endregion
+
+    // #region Longest Repeating Character Replacement
+
+    // Space (N) Time (N)
+    public int characterReplacement(String s, int k) {
+        int maxSub = 0, maxLen = 0, len = s.length(), left = 0;
+        Map<Character, Integer> m = new HashMap<Character, Integer>();
+
+        for (int end = 0; end < len; end++) {
+            char curr = s.charAt(end);
+            m.put(curr, m.getOrDefault(curr, 0) + 1);
+            maxSub = Math.max(maxSub, m.get(curr));
+
+            if (end - left + 1 - maxSub > k) {
+                char leftChar = s.charAt(left);
+                m.put(leftChar, m.get(leftChar) - 1);
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, end - left + 1);
+        }
+        return maxLen;
     }
 
     // #endregion
