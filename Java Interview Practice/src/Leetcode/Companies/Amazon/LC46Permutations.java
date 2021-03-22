defbb3af85fb3afe82d03f26e0dc09aed62de6d5
Leo Le <leolemain@gmail.com>
2021-03-21 22:25:03 -0700

    Added 46 permutation, check point for anagram in string

--- Java Interview Practice/src/Leetcode/Companies/Amazon/LC46Permutations.java
+++ Java Interview Practice/src/Leetcode/Companies/Amazon/LC46Permutations.java
@@ -1,66 +1,109 @@
 package src.Leetcode.Companies.Amazon;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Set;
 
 public class LC46Permutations {
 
+    /**
+     * Space (N * N!) - required space to store each permutations found Time (N *
+     * N!) - N is the height of the tree required to get the permutation - N! is the
+     * amount of permutations possible
+     */
     public List<List<Integer>> permute(int[] nums) {
 
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 1)
+            return result;
 
+        List<Integer> rootLevel = new LinkedList<>();
+        rootLevel.add(nums[0]);
+        int numPtr = 1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-
-    public static List<List<Integer>> posPermutation(int[] arr) {
-        List<List<Integer>> result = new LinkedList<List<Integer>>();
-        if (arr.length < 1 || arr == null) {
+        if (nums.length == 1) {
+            result.add(rootLevel);
             return result;
         }
-        LinkedList<Integer> currLevel = new LinkedList<>();
+
         Queue<List<Integer>> q = new LinkedList<>();
-        currLevel.offer(1);
-        q.offer(currLevel);
-        int ptr = 1;
+        q.offer(rootLevel);
 
-        while (!q.isEmpty() && ptr < arr.length) {
+        while (!q.isEmpty() && numPtr < nums.length) {
             int size = q.size();
+
+            // Traverse through all permutations of this level
             for (int i = 0; i < size; i++) {
+                List<Integer> currLevel = q.poll();
 
-                List<Integer> curr = q.poll();
+                // Add new nums integer into each indicies location
+                // The size + 1 is because a new index of nums is being added
+                for (int j = 0; j < currLevel.size() + 1; j++) {
 
-                for (int j = 0; j < curr.size() + 1; j++) {
-                    LinkedList<Integer> newLvl = new LinkedList<>(curr);
-                    newLvl.add(j, arr[ptr]);
-                    q.offer(newLvl);
+                    // Get a copy of curr level and create a new
+                    // permutation with the added int at index j
+                    List<Integer> newLevel = new LinkedList<>(currLevel);
+                    newLevel.add(j, nums[numPtr]);
+                    q.offer(newLevel);
 
-                    if (ptr == arr.length - 1) {
-                        result.add(newLvl);
+                    // This is the last index of nums array (correct permutation level)
+                    if (numPtr == nums.length - 1) {
+                        result.add(newLevel);
                     }
                 }
             }
-            ptr++;
+            numPtr++;
         }
+
         return result;
     }
 
+    /**
+     * Space (N * N!) - With N is the height of the tree required to traverse until reach the permutation 
+     *                - N! is the amount of permutations (backtracking)
+     * 
+     * Time (N * N!) - With N is the height of the tree to get the permutation
+     *               - N! is the number of permutations
+     */
+    public List<List<Integer>> permuteDFS(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (nums.length < 1 || nums == null)
+            return result;
+
+        dfsHelper(result, new LinkedList<>(), new HashSet<>(), nums);
+
+        return result;
+    }
+
+    private void dfsHelper(List<List<Integer>> result, List<Integer> currLevel, Set<Integer> levelSet, int[] nums) {
+        // At the correct permutation level
+        if (levelSet.size() == nums.length)
+        {
+            // Make sure to make a copy of curr list if not, the reference will be messed up
+            result.add(new LinkedList<>(currLevel));
+            return;
+        }
+
+        // Go thru all number in nums
+        for (int i = 0; i < nums.length; i++) {
+            // Skip existed number to add the next number
+            if (levelSet.contains(nums[i])) continue;
+
+            // Add set to track, add list to return result
+            levelSet.add(nums[i]);
+            currLevel.add(nums[i]);
+
+            // Recursively move forward to the next number
+            dfsHelper(result, currLevel, levelSet, nums);
+
+            // Remove the number added from the recursion to backtrack to a new permutation
+            int numberFromPrevDFS = currLevel.get(currLevel.size() - 1);
+            levelSet.remove(numberFromPrevDFS);
+            currLevel.remove(currLevel.size() - 1);
+        }
+
+    }
 }
