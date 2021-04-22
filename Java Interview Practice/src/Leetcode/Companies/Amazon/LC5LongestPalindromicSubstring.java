551972139f01680d248437eea515fedb4336f793
Leo Le <leolemain@gmail.com>
2021-04-21 23:50:36 -0700

    added LC5 and 88

--- Java Interview Practice/src/Leetcode/Companies/Amazon/LC5LongestPalindromicSubstring.java
+++ Java Interview Practice/src/Leetcode/Companies/Amazon/LC5LongestPalindromicSubstring.java
@@ -1,12 +1,32 @@
 public class LC5LongestPalindromicSubstring {
 
+    private int lo, maxLen;
+
     /**
-     * Space ()
+     * Space (N^2)
+     * 
      * Time ()
      */
     public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
 
-        return null;
+        for (int i = 0; i < len - 1; i++) {
+            extendPalindrome(s, i, i); // assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i + 1); // assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
     }
 
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
 }
--- Java Interview Practice/src/Leetcode/Problems/LC88MergeSortedArray.java
+++ Java Interview Practice/src/Leetcode/Problems/LC88MergeSortedArray.java
@@ -5,18 +5,27 @@ import java.util.Arrays;
 public class LC88MergeSortedArray {
 
     /**
-     * Space ()
-     * Time ()
+     * Space (1)
+     * Time (N)
      */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Use pointers at the end of each arr
+        // Use m and n since these are the amount of actual number which not including 0
+        int tail1 = m - 1;
+        int tail2 = n - 1;
+        int finished = m + n - 1;
 
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        int ptr2 = 0;
+        // loop until either tail reach the start of the arr
+        while (tail1 >= 0 && tail2 >= 0) {
+            nums1[finished--] = (nums1[tail1] > nums2[tail2]) ? 
+                nums1[tail1--] : 
+                nums2[tail2--];
+        }
 
-        if (len1 < 1 || len2 < 1) return;
-        
-
-
+        // only need to combine with remaining nums2
+        while (tail2 >= 0) {
+            nums1[finished--] = nums2[tail2--];
+        }
     }
+
 }
