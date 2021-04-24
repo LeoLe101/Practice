dbe6aa9fc66842f23eebb44b3d1572cff3977267
LeoLe101 <leolemain@gmail.com>
2021-04-23 18:18:23 -0700

    Added 101, 100

--- Java Interview Practice/src/Leetcode/Companies/Amazon/LC101SymmetricTree.java
+++ Java Interview Practice/src/Leetcode/Companies/Amazon/LC101SymmetricTree.java
@@ -3,13 +3,21 @@ package src.Leetcode.Companies.Amazon;
 import src.Util.TreeNode;
 
 public class LC101SymmetricTree {
-    
-    /**
-     * Space ()
-     * 
-     * Time ()
-     */
-    public boolean isSymmetric(TreeNode root) {
 
-    }
+  /**
+   * Space (N) due to the stack needed for the recursion
+   * 
+   * Time (N) traverse through all the node within the tree
+   */
+  public boolean isSymmetric(TreeNode root) {
+    return root == null || helper(root.left, root.right);
+  }
+
+  private boolean helper(TreeNode left, TreeNode right) {
+    if (left == null || right == null)
+      return left == right;
+    if (left.val != right.val)
+      return false;
+    return helper(left.left, right.right) && helper(left.right, right.left);
+  }
 }
